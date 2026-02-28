963b0acadcf6725f099b38ae8e820a2781fcb75f
amillanaol <alexis.millanao@hotmail.com>
2026-02-28 00:40:32 -0300

    refactor(settings): improve env var handling

--- src/infrastructure/django_app/settings/production.py
+++ src/infrastructure/django_app/settings/production.py
@@ -4,15 +4,19 @@ from .base import *  # noqa: F401, F403
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = [
+    host for host in os.environ.get("ALLOWED_HOSTS", "").split(",") if host
+]
 
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = os.environ.get("SECURE_SSL_REDIRECT", "True") == "True"
+SECURE_SSL_REDIRECT = os.environ.get("SECURE_SSL_REDIRECT", "False") == "True"
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
-CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "").split(",")
+CORS_ALLOWED_ORIGINS = [
+    origin for origin in os.environ.get("CORS_ALLOWED_ORIGINS", "").split(",") if origin
+]
--- src/infrastructure/django_app/urls.py
+++ src/infrastructure/django_app/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
 urlpatterns = [
+    path("favicon.ico", lambda request: HttpResponse(status=204)),
     path("admin/", admin.site.urls),
     path("api/", include("src.infrastructure.web.api.urls")),
 ]
